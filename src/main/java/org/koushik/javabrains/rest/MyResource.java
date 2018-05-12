383d5580f5773b1145597530e0fd383a7e955349
alamnr <alamnr@gmail.com>
2018-05-13 00:20:10 +0600

    Commit for Advance JAX-RS-07-Param-Annotation-and-Member-variables

--- src/main/java/org/koushik/javabrains/rest/MyResource.java
+++ src/main/java/org/koushik/javabrains/rest/MyResource.java
@@ -3,24 +3,34 @@ package org.koushik.javabrains.rest;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 //Life cycle of MyResource is in request scope,
 //i.e: each time you make a request an instance of MyResource.java is created
 //and after serving the response the MyResource.java instance being destroyed and garbage collected
 
-@Path("test")
-@Singleton
+@Path("{pathParam}/test")
+//@Singleton
+// You can't declare a resource singleton having member variable with annotation @PathParam and @QueryParam
+// cause in singleton mode the resource is instantiated while application is started up
+// and at them time those member variable with @PathParam and @QueryParam annotation can't be initialised
+// hence exception thrown, validation failed
+
 public class MyResource {
 	
-	private int count;
+	
+	@PathParam("pathParam")private String pathParamExample;
+	@QueryParam("query")private String queryParamExample;
+	
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String testMethod(){
-		count =  count+1;
-		return "It Works! The  Method wass called " +count + " time(s)";
+		
+		return "It Works! Path Param is -" + pathParamExample + " and Query param is- "+ queryParamExample;
 	}
 
 }
