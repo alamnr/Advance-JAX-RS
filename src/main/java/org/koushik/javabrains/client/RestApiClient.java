57c5f42cbec8c9df21f9653822510a7357343378
alamnr <alamnr@gmail.com>
2018-05-13 10:09:39 +0600

    Commit for Advance JAX-RS-15 Client Code Explained

--- src/main/java/org/koushik/javabrains/client/RestApiClient.java
+++ src/main/java/org/koushik/javabrains/client/RestApiClient.java
@@ -1,7 +1,12 @@
 package org.koushik.javabrains.client;
 
+
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.server.ClientBinding;
@@ -13,9 +18,12 @@ public class RestApiClient {
 		
 		Client client = ClientBuilder.newClient();
 		
-		Response response=client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/1")
-			  .request().get();
-		Message msg = response.readEntity(Message.class);
+		WebTarget target =client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/1");
+		
+		Builder builder = target.request(MediaType.APPLICATION_JSON);
+		//Message msg = builder.get(Message.class);
+		String msg = builder.get(String.class);
+		 
 		System.out.println(msg.toString());
 		
 		
