a03c5bdabc057ef5265a3c07d470cba7ef9db3fc
alamnr <alamnr@gmail.com>
2018-05-13 09:01:37 +0600

    Commit for Advance JAX-RS-11-Implementing_Messagebody writere

--- src/main/java/org/koushik/javabrains/rest/MyResource.java
+++ src/main/java/org/koushik/javabrains/rest/MyResource.java
@@ -1,5 +1,8 @@
 package org.koushik.javabrains.rest;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,25 +15,16 @@ import javax.ws.rs.core.MediaType;
 //i.e: each time you make a request an instance of MyResource.java is created
 //and after serving the response the MyResource.java instance being destroyed and garbage collected
 
-@Path("{pathParam}/test")
-//@Singleton
-// You can't declare a resource singleton having member variable with annotation @PathParam and @QueryParam
-// cause in singleton mode the resource is instantiated while application is started up
-// and at them time those member variable with @PathParam and @QueryParam annotation can't be initialised
-// hence exception thrown, validation failed
+@Path("test")
 
 public class MyResource {
 	
 	
-	@PathParam("pathParam")private String pathParamExample;
-	@QueryParam("query")private String queryParamExample;
-	
-	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String testMethod(){
+	public Date testMethod(){
 		
-		return "It Works! Path Param is -" + pathParamExample + " and Query param is- "+ queryParamExample;
+		return Calendar.getInstance().getTime();
 	}
 
 }
